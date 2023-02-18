dfec40bfa1716043875f2809d05234ca487ceabc
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-02-18 12:22:01 +0300

    Add new function ContainsInt

--- utils.go
+++ utils.go
@@ -9,3 +9,13 @@ func Contains(str []string, x string) bool {
 
 	return false
 }
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+
+	return false
+}
