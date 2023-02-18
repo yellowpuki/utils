6f146b6417e2888c08b471bcbb508fa452c2e68b
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-02-18 14:40:51 +0300

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yellowpuki/utils/v2
+module github.com/yellowpuki/utils/v3
 
 go 1.20
--- utils.go
+++ utils.go
@@ -10,7 +10,7 @@ func InSlice(str []string, x string) bool {
 	return false
 }
 
-func ContainsInt(a []int, x int) bool {
+func InSliceInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
